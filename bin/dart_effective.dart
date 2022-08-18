e0b4900617136fd26972ee64b712ef66277e9db0
Candra Roseahansyah <92659587+Hansrosean@users.noreply.github.com>
2022-08-18 23:26:32 +0700

    Add files via upload

---
+++ bin/dart_effective.dart
@@ -0,0 +1,5 @@
+import 'package:dart_effective/dart_effective.dart' as dart_effective;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_effective.calculate()}!');
+}
---
+++ test/dart_effective_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_effective/dart_effective.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
